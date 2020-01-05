b3bef7b9bd1c1bf0012935b490d01ef80c86dd0e
Kim Joon <jk1094@naver.com>
2020-01-05 16:21:03 +0900

    Insertion Sort

--- insertionSort.java
+++ insertionSort.java
@@ -1,5 +1,32 @@
+/**
+ *  삽입 정렬
+ *  오름차순 정렬
+ */
 public class insertionSort{
     public static void main(String[] args) {
+        int arrays[] = {8,5,6,2,4,3,10,12};
+        Insertion selection = new Insertion();
+        selection.sort(arrays);
+        for(int num : arrays){
+            System.out.print(num + " ");
+        }
+    } 
+}
+class Insertion{
+    public void sort(int[] data){
+        int order = 1;
+        int num = 1;
         
+        while(order<data.length){
+            for(int i=0; i<num; i++){
+                int key = data[order];
+                if(key<data[i]){
+                    data[order] = data[i];
+                    data[i] = key;
+                }
+            }
+            order++;
+            num++;
+        }
     }
 }
